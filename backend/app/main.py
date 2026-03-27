802422f0f199ebac5185b368fd16b25658ab508e
anelya <135839065+AnelyaKurbanova@users.noreply.github.com>
2026-03-27 21:19:41 +0500

    auth test

--- backend/app/main.py
+++ backend/app/main.py
@@ -115,6 +115,7 @@ app.add_middleware(
         "https://www.orenaitest.app",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://192.168.0.105:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
